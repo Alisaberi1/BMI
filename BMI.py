2098276b0b20a8e90641ab769b5e4959f48b03e4
Ali $@ <90723218+Alisaberi1@users.noreply.github.com>
2021-11-14 18:34:40 +0330

    BMI
    Body mass index (BMI) is a measure of body fat based on height and weight that applies to adult men and women.

---
+++ BMI.py
@@ -0,0 +1,29 @@
+a=int(input("you're weight in number:"))
+b=int(input("you're high in number:"))
+
+print(f"you're weight is {a} kg")
+print(f"you're high  is {b}meter")
+c=print(float(b/a**2))
+
+for c in range(1,):
+
+	while(c) != None and c<18.5:
+
+
+		print("you're under weight")
+		break	
+
+	while(c) != None and 18.5<=c<25:
+
+		print("you're normal")
+		break	
+
+	while(c) != None and 25<=c<30:
+
+		print(" you're overweight")
+		break
+
+	while(c) != None and 30<=c:
+
+		print("you're obese")
+		break
